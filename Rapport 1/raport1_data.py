f68d380272ac7017873e854dbcfb0a01dca1bf27
nicholaskarlsen <nichoka@student.matnat.uio.no>
2018-02-08 20:19:08 +0100

    changes were made

--- Rapport 1/raport1_data.py
+++ Rapport 1/raport1_data.py
@@ -4,9 +4,13 @@ import matplotlib.pyplot as plt
 periods = np.array([
     14.14, 14.39, 14.39, 14.40, 14.29, 14.39, 14.29, 14.34, 14.20, 14.52
     ])
-
+print "Stopwatch:"
 print "Mean period", np.mean(periods)
 print "standard dev", np.std(periods)
 
-#plt.plot(periods, "x")
-#plt.show()
+periods2 = np.array([
+    116, 121, 128])
+
+print "\nHourglass:"
+print "Mean period", np.mean(periods2)
+print "standard dev", np.std(periods2)
