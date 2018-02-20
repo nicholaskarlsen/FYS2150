5d246573549660f53afaf97f465c8bbb7c64a432
nicholaskarlsen <nichoka@student.matnat.uio.no>
2018-02-20 23:29:07 +0100

    add

--- lab5/termistortemp.py
+++ lab5/termistortemp.py
@@ -1,13 +1,21 @@
-import matplotlib.pyplot as plt
-import numpy as np
-"""
+# By Nicholas Karlsen
 import scipy.io as sio
 import numpy as np
 
-data = sio.loadmat("linjedata.mat")
 
-x = data.get("x")
-"""
+def importdata(filename):
+    "Used to import data from .m file"
+    data = sio.loadmat(filename)
+    R = data.get("R")  # Change R to variable name in .m file
+    return R
+
 
 def Temp(R):
-    return 1.0 / a + b*np.log(R) + c*(np.log(R))**3
+    a = 8.420e-4
+    b = 2.0868e-4
+    c = 8.591e-8
+    return 1.0 / (a + b * np.log(R) + c * (np.log(R))**3)
+
+
+if __name__ == '__main__':
+    print Temp([1e6, 20e3])
