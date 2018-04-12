591aea475a341e6faba43aefea04cbf2517b670d
nicholaskarlsen <nichoka@student.matnat.uio.no>
2018-04-12 19:42:23 +0200

    mmm

--- Rapport 3/scripts/lab_data.py
+++ Rapport 3/scripts/lab_data.py
@@ -13,23 +13,6 @@ import FYS2150lib as fys
 
 rcParams.update({'font.size': 13})  # Sets font size of plots
 
-
-def E_sound(f, L, d, M):
-    '''
-    Returns youngs modulus given
-    f = root frequency
-    L = lenght of rod
-    d = diameter of rod
-    M = mass of rod
-    '''
-    return (16.0 * M * L * f**2) / (np.pi * d**2)
-
-
-def E_sound_error(E, sd, sf, sL, sM, d, f, L, M):
-    return E * np.sqrt((2 * sd / d)**2 + (2 * sf / f)**2 +
-                       (2 * sL / L)**2 + (2 * sM / M)**2)
-
-
 def weight_data(set=1):
     "set decides which data set the function returns."
     set = set.lower()   # Forces lowercase
@@ -53,14 +36,68 @@ def weight_data(set=1):
     if set == sets[0]:  # Return corrected masses
         return m_a, m_b, m_c
 
+    m_rod_ring = np.array([2482.7, 2482.5, 2482.1]) * 1e-3
+    m_ring = 34.4 * 1e-3    #kg
+    m_rod = (mean(m_rod_ring) - m_ring - b) / a  #kg
+
+
     if set == sets[1]:
-        return
+        return m_rod
 
     if set not in sets:
         print "Invalid set"
         print "List of valid sets:", sets
         print "exiting..."
         exit()
+
+
+def E_sound(f, L, d, M):
+    '''
+    Returns youngs modulus given
+    f = root frequency
+    L = lenght between knives
+    d = diameter of rod
+    M = mass of rod
+    '''
+    return (16.0 * M * L * f**2) / (np.pi * d**2)
+
+
+def E_sound_error(E, sd, sf, sL, sM, d, f, L, M):
+    return E * np.sqrt((2 * sd / d)**2 + (2 * sf / f)**2 +
+                       (2 * sL / L)**2 + (2 * sM / M)**2)
+
+
+d = np.array([15.98, 15.99, 15.99, 16.00,
+              15.99, 15.99, 15.98, 15.99,
+              15.99, 15.99]) * 1e-3
+d_mean = np.mean(d)
+d_err = np.sqrt(fys.stddev(d)[1]**2 + (0.01e-3)**2) # Std dev of mean + instrumentation error
+
+f_root = 1213.72
+f_err = 0.04  # resolution of FFT
+L_err = 1e-3
+M_err = 1e-3
+
+l_BC_outer = 133.9 * 1e-2
+l_knife_diameter = 4.09 * 1e-3
+l_BC = l_BC_outer - l_knife_diameter
+
+E_sound = E_sound(f=f_root,
+                  L=l_BC,
+                  d=d_mean,
+                  M=weight_data("rod"))
+
+print "E from root f = %e" % E_sound
+
+print "E_err root = %e" % E_sound_error(E=E_sound,
+                                        sd=d_err,
+                                        sf=f_err,
+                                        sL=L_err,
+                                        sM=M_err,
+                                        d=d_mean,
+                                        f=f_root,
+                                        L=l_BC,
+                                        M=weight_data("rod"))
 
 
 def experiment1_data():
@@ -82,10 +119,10 @@ def experiment1_data():
 
     h_mean = (h_1 + h_2 + h_3 + h_4 + h_5) / 5.0
 
-    m, c, dm, dc = fys.linfit(mass_dat, h_mean)
+    A, B, dA, dB = fys.linfit(mass_dat, h_mean)
 
     mass = linspace(0, 3.5, 8)
-    h_mass = m * mass + c  # h(m)
+    h_mass = A * mass + B  # h(m)
 
 
     def plotdata():
@@ -100,7 +137,7 @@ def experiment1_data():
         ylabel("h(m) [m]")
         ticklabel_format(style='sci', axis='y', scilimits=(0,0))
         legend()
-        title("Linear fit of mean deflection data; h(m) = Am + B\n$\delta A =$ %.2e" % dm)
+        title("Linear fit of mean deflection data; h(m) = Am + B\n$\delta A =$ %.2e" % dA)
         savefig("figs/h_m_fig.png")
         close()
     plotdata()
@@ -115,9 +152,6 @@ def experiment1_data():
                                              h_3[i],
                                              h_4[i],
                                              h_5[i]]))[0]
-            print i
-        print deviation
-        print len(mass_dat), len(deviation)
         plot(mass_dat, deviation, linestyle="--")
         plot(mass_dat, deviation, "o")
         ticklabel_format(style='sci', axis='y', scilimits=(0,0))
@@ -140,16 +174,13 @@ def experiment1_data():
     #Maalinger av stangens diameter d paa forskjellige punkter
     # Moore & Wright 1965 MI +- 0.01mm
     d = array([15.98, 15.99, 15.99, 16.00, 15.99, 15.99, 15.98, 15.99, 15.99, 15.99]) * 1e-3  # [m]
-    d_m = mean(d); #m
+    d_mean = mean(d) #m
 
-    A = abs((h_mass - c) / mass)
-
-    E = mean(4.0 * l**3 * const.g / (3 * pi * A * d_m**4)[1:-1])
-    print E
+    E = (4.0 * l**3 * const.g / (3 * pi * abs(A) * d_mean**4))
+    print "E from deflection = %e"%E
 
 def experiment_2():
     ''' Data pertaining to the audio exp.'''
 
 
-if __name__ == "__main__":
-    experiment1_data()
+experiment1_data()
