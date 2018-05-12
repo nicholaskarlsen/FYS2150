0e34847467a941b7b2d10fcec68565f014a4c870
Nicholas <nichoka@student.matnat.uio.no>
2018-05-12 21:25:54 +0200

    ..

--- Rapport 5/scripts/zeemanread.py
+++ Rapport 5/scripts/zeemanread.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-
+Finds diameter of diffraction rings from Zeeman experiment
+in the FYS2150 Waveoptics lab
 '''
 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
-from skimage import measure
 from scipy import ndimage
 # from PIL import Image
 # import skimage.morphology as morph
@@ -16,7 +16,7 @@ from scipy import ndimage
 
 def rgb2gray(rgb):
     '''
-    Converts shape=(N,M,rgb) array to (N, M) grayscale array see wiki page
+    Converts shape=(N,M,rgb) array to (N, M) grayscale array
     '''
     return np.dot(rgb[..., :3], [0.299, 0.587, 0.114]).astype(int)
 
@@ -25,7 +25,7 @@ def gray2binary(gray, limBW=128):
     """Converts grayscale image to binary grayscale of 0 OR 255
     image must be array of shape=(N, M)
     gray: (N, M) array
-    limBW:
+    limBW: threshhold limit between B/W
     """
     bw = np.asarray(gray).copy()
     bw[bw < limBW] = 0      # Black
@@ -45,13 +45,33 @@ def readZeeman(filename):
     plt.imshow(bwImg, cmap=plt.get_cmap('gray'))
     plt.close()
     plt.axhline(0, linestyle="-", color="r")
-    plt.imshow(binCrop,cmap=plt.get_cmap('gray'))
+    plt.imshow(binCrop, cmap=plt.get_cmap('gray'))
     plt.close()
 
     edge_horizont = ndimage.sobel(binCrop, 0)
     edge_vertical = ndimage.sobel(binCrop, 1)
     magnitude = np.hypot(edge_horizont, edge_vertical)
+    outlines = gray2binary(magnitude, 20)
+    plt.imshow(outlines, cmap=plt.get_cmap("gray"))
+    print np.shape(outlines)
 
-    plt.imshow(magnitude, cmap=plt.get_cmap("gray"))
+    outline_indeces = []
+
+    for i in range(len(outlines)):
+        outline_indeces_row = [0]  # Setting first element to zero to make loop work
+        for j in range(len(outlines[i])):
+            if outlines[i, j] == 0:
+                pass
+            else:
+                if abs(j - outline_indeces_row[-1]) < 4:
+                    pass
+                else:
+                    outline_indeces_row.append(j)
+        outline_indeces_row.pop(0)  # remove the zero
+        outline_indeces.append(outline_indeces_row)
+    print outline_indeces[30]
+    plt.plot(outline_indeces[30], np.zeros_like(outline_indeces[30]) + 30, "rx")
     plt.show()
+
+
 readZeeman("figs/ZEEMAN4A.jpg")
