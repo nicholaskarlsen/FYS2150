0220effa2cc878810d73790114d482614889ce83
Nicholas <nichoka@student.matnat.uio.no>
2018-05-19 20:47:16 +0200

    ...

--- Rapport 6/scripts/reflection.py
+++ Rapport 6/scripts/reflection.py
@@ -5,24 +5,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def read_capfile(filename):
-	capfile = open(filename, "r")
+    capfile = open(filename, "r")
 
-	time = []
-	angle = []; intensity = []
+    time = []
+    angle = []
+    intensity = []
+    capfile.readline()
+    print capfile.readline().split()[:4]
+    for i in range(2):
+        capfile.readline()
+    for line in capfile:
+        col = line.split()
+        time.append(col[0].replace(",", "."))
+        intensity.append(col[1].replace(",", "."))
+        angle.append(col[2].replace(",", "."))
+    capfile.close()
 
-	for i in range(4):
-		capfile.readline()
-	for line in capfile:
-		col = line.split()
-		time.append(col[0])
-		intensity.append(col[1])
-		angle.append(col[2])
-	capfile.close()
 
-	return time, angle, intensity
+    angle = np.array(angle, dtype=float)
+    intensity = np.array(intensity, dtype=float)
+    time = np.array(time, dtype=float)
 
-time1, angle1, intensity1 = read_capfile("data/run1_s_polarisert.txt")
+    return time, angle, intensity
 
-plt.plot(time1, angle1, "x")
+
+time1, angle1, intensity1 = read_capfile("data/run1_p_polarisert.txt")
+
+plt.plot(np.rad2deg(angle1[:-1]), intensity1[:-1], linestyle="--", color="black")
+plt.plot(np.rad2deg(angle1[:-1]), intensity1[:-1], ".", color="red")
+plt.xlabel("Angle [deg]")
+plt.ylabel("Intensity [%]")
+plt.title("P-Polarized light")
+plt.grid("on")
+plt.show()
+
+time2, angle2, intensity2 = read_capfile("data/run1_s_polarisert.txt")
+time3, angle3, intensity3 = read_capfile("data/run2_s_polarisert.txt")
+
+plt.plot(np.rad2deg(angle2), intensity2, "r.", label="Run #1")
+plt.plot(np.rad2deg(angle3), intensity3, "b.", label="Run #2")
+plt.xlabel("Angle [deg]")
+plt.ylabel("Intensity [%]")
+plt.title("S-Polarized light")
+plt.grid("on")
+plt.legend()
 plt.show()
